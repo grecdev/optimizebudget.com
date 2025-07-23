a4bb7c4c10c4d17daa4596e1ea44f25ea42ee48d
Grecu Alexandru <grecualexandru001@gmail.com>
2025-07-23 14:15:56 +0300

    fix(OBT-1): import different module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NgIf],
   providers: [],
   bootstrap: [AppComponent],
 })
