461adc2ce1931ff73fe5f4525239dddd47088f3b
Grecu Alexandru <grecualexandru001@gmail.com>
2025-07-31 09:30:07 +0300

    feat(svg-icon): addSvgIconConfig method for registry

--- src/app/shared/components/icon/icon-registry.service.ts
+++ src/app/shared/components/icon/icon-registry.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { of } from 'rxjs';
+
+import type { SvgIconConfig, AddSvgIconConfigOptions } from './icon.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class IconRegistryService {
   constructor() {}
+
+  private _svgIconConfigs = new Map<string, SvgIconConfig>();
+
+  getSvg(iconName: string) {
+    return of('<svg></svg>');
+  }
+
+  /**
+   * @summary Registers an icon config by name
+   *
+   * @param {AddSvgIconConfigOptions} options - Required options to add the new svg icon,
+   *
+   * @returns {void}
+   */
+  addSvgIconConfig(options: AddSvgIconConfigOptions) {
+    const { name, config } = options;
+
+    this._svgIconConfigs.set(name, config);
+  }
 }
