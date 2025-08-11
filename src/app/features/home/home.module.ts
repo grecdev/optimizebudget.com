081b013d27192ddfb05e3f313a01d135c4c31aa0
Grecu Alexandru <grecualexandru001@gmail.com>
2025-08-11 17:49:00 +0300

    feat(layout): widget box border styling

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { WidgetBoxModule } from '@core/layout/widget-box/widget-box.module';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, WidgetBoxModule],
 })
 export class HomeModule {}
