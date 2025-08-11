7bceab927f1616fc3d81588b9709ff1848b9741c
Grecu Alexandru <grecualexandru001@gmail.com>
2025-08-11 18:35:14 +0300

    fix(layout): ng-content tag, widget-box title

--- src/app/core/layout/widget-box/widget-box.component.ts
+++ src/app/core/layout/widget-box/widget-box.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-widget-box',
   templateUrl: './widget-box.component.html',
-  styleUrls: ['./widget-box.component.scss']
+  styleUrls: ['./widget-box.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'widget-box',
+  },
 })
-export class WidgetBoxComponent {
-
-}
+export class WidgetBoxComponent {}
