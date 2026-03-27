ab21505e40dc8ec8666fe648a22776ab101b470b
Grecu Alexandru <grecualexandru001@gmail.com>
2026-03-27 09:29:57 +0200

    feat(OBT-44): render categories inside select element

--- src/app/features/expenses/add-expense-dialog/expense-dialog-body/expense-dialog-body.component.ts
+++ src/app/features/expenses/add-expense-dialog/expense-dialog-body/expense-dialog-body.component.ts
@@ -1,6 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { CategoryType } from '@shared/models/enums';
+
 import { type AppOverlayContentInstances } from '@shared/components/overlay/overlay.model';
+
+import {
+  type ExpenseCategoriesFormatted,
+  type ExpenseCategoryItem,
+} from './expense-dialog-body.model';
 
 @Component({
   selector: 'expense-dialog-body',
@@ -18,6 +25,27 @@ export class ExpenseDialogBodyComponent implements AppOverlayContentInstances {
    */
   public overlayReference: AppOverlayContentInstances['overlayReference'] = null;
 
+  /**
+   * @summary - Category to set
+   *
+   * @type {ExpenseCategoriesFormatted}
+   *
+   * @public
+   */
+  public expenseCategories: ExpenseCategoriesFormatted = [];
+
+  constructor() {
+    this._formatCategoriesObject();
+  }
+
+  /**
+   * @summary - Close expense dialog on custom button click.
+   *
+   * @param {MouseEvent} event - DOM Event
+   *
+   * @public
+   * @returns {void}
+   */
   public closeExpenseDialog(event: MouseEvent): void {
     event.stopPropagation();
 
@@ -26,5 +54,31 @@ export class ExpenseDialogBodyComponent implements AppOverlayContentInstances {
     }
 
     this.overlayReference.close();
+  }
+
+  /**
+   * @summary - Create an array with key-value pair objects.
+   *
+   * @private
+   * @returns {void}
+   */
+  private _formatCategoriesObject(): void {
+    this.expenseCategories = Object.values(CategoryType).map(item => ({
+      textContent: item,
+      value: item.toLowerCase(),
+    }));
+  }
+
+  /**
+   * @summary - Track by function used for expense categories for loop.
+   *
+   * @param {number} index - Not used really.
+   * @param {ExpenseCategoryItem} item - Array's item.
+   *
+   * @returns {string}
+   * @public
+   */
+  public expenseCategoriesTrackByFn(_: number, item: ExpenseCategoryItem): string {
+    return item.value;
   }
 }
