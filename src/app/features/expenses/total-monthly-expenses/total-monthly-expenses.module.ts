463f02e6240e069db54cc7bafdaa2c280efa5e76
Grecu Alexandru <grecualexandru001@gmail.com>
2026-03-27 10:52:20 +0200

    feat(OBT-45): row container for widget boxes

--- src/app/features/expenses/total-monthly-expenses/total-monthly-expenses.module.ts
+++ src/app/features/expenses/total-monthly-expenses/total-monthly-expenses.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { WidgetBoxModule } from '@core/layout/widget-box/widget-box.module';
+
 import { TotalMonthlyExpensesComponent } from './total-monthly-expenses.component';
 
 @NgModule({
   declarations: [TotalMonthlyExpensesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, WidgetBoxModule],
   exports: [TotalMonthlyExpensesComponent],
 })
 export class TotalMonthlyExpensesModule {}
