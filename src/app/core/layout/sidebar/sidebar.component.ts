f86abba1871d7bb17d19b577c442a7931fb060cb
Grecu Alexandru <grecualexandru001@gmail.com>
2026-05-28 18:11:49 +0300

    fix(app-sidebar): optimized change detection

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   host: {
     class: 'sidebar',
   },
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {}
