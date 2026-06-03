731a17192320d13370e2a6d20feb0a6d81f827a5
Grecu Alexandru <grecualexandru001@gmail.com>
2026-06-03 19:38:33 +0300

    feat(OBT-37): MediaQueryService to dynamically render the components

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,7 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+
 import { DomSanitizer } from '@angular/platform-browser';
 
+import { Subject, takeUntil } from 'rxjs';
+
 import { IconRegistryService } from '@shared/components/icon/icon-registry.service';
+import { MediaQueryService } from '@shared/services/media-query/media-query.service';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +19,70 @@ import { IconRegistryService } from '@shared/components/icon/icon-registry.servi
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Hello world!';
 
-  constructor(iconRegistryService: IconRegistryService, sanitizer: DomSanitizer) {
+  /**
+   * @summary - Based on this member, render our components, inside the template.
+   *
+   * @type {boolean}
+   *
+   * @public
+   */
+  public isMobile: boolean = false;
+
+  private readonly _changeDetectorRef: ChangeDetectorRef;
+  private readonly _mediaQueryService: MediaQueryService;
+
+  /**
+   * @summary - Subject to unsubscribe on component destroy.
+   *
+   * @type {Subject<void>}
+   *
+   * @private
+   * @readonly
+   */
+  private readonly _mediaQueryDestroy$: Subject<void> = new Subject();
+
+  constructor(
+    iconRegistryService: IconRegistryService,
+    sanitizer: DomSanitizer,
+    mediaQueryService: MediaQueryService,
+    changeDetectorRef: ChangeDetectorRef
+  ) {
     iconRegistryService.addSvgIconConfig({
       name: 'basic-notification-icon',
       url: sanitizer.bypassSecurityTrustResourceUrl('assets/icons/basic-notification-icon.svg'),
     });
+
+    this._mediaQueryService = mediaQueryService;
+    this._changeDetectorRef = changeDetectorRef;
+  }
+
+  /**
+   * @summary - Init the media query subscription.
+   *
+   * @returns {void}
+   * @private
+   */
+  private _initMediaQuerySubscription(): void {
+    this._mediaQueryService
+      .mediaQuery('max', 'md')
+      .pipe(takeUntil(this._mediaQueryDestroy$))
+      .subscribe({
+        next: value => {
+          this.isMobile = value;
+
+          this._changeDetectorRef.markForCheck();
+        },
+      });
+  }
+
+  ngOnInit(): void {
+    this._initMediaQuerySubscription();
+  }
+
+  ngOnDestroy(): void {
+    this._mediaQueryDestroy$.complete();
   }
 }
