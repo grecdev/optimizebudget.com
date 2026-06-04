0cc36d3aebbffbb2b230639bd01898afcec6b642
Grecu Alexandru <grecualexandru001@gmail.com>
2026-06-04 15:17:48 +0300

    fix(OBT-37): actually i don't need to unsubscribe from media query

--- src/app/core/layout/header/user-avatar/user-avatar.component.ts
+++ src/app/core/layout/header/user-avatar/user-avatar.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 
-import { Subject, takeUntil } from 'rxjs';
-
 import { MediaQueryService } from '@shared/services/media-query/media-query.service';
 
 @Component({
@@ -25,16 +23,6 @@ export class UserAvatarComponent {
   private readonly _changeDetectorRef: ChangeDetectorRef;
   private readonly _mediaQueryService: MediaQueryService;
 
-  /**
-   * @summary - Subject to unsubscribe on component destroy.
-   *
-   * @type {Subject<void>}
-   *
-   * @private
-   * @readonly
-   */
-  private readonly _mediaQueryDestroy$: Subject<void> = new Subject();
-
   constructor(mediaQueryService: MediaQueryService, changeDetectorRef: ChangeDetectorRef) {
     this._mediaQueryService = mediaQueryService;
     this._changeDetectorRef = changeDetectorRef;
@@ -47,23 +35,16 @@ export class UserAvatarComponent {
    * @private
    */
   private _initMediaQuerySubscription(): void {
-    this._mediaQueryService
-      .mediaQuery('max', 'md')
-      .pipe(takeUntil(this._mediaQueryDestroy$))
-      .subscribe({
-        next: value => {
-          this.isMobile = value;
+    this._mediaQueryService.mediaQuery('max', 'md').subscribe({
+      next: value => {
+        this.isMobile = value;
 
-          this._changeDetectorRef.markForCheck();
-        },
-      });
+        this._changeDetectorRef.markForCheck();
+      },
+    });
   }
 
   ngOnInit(): void {
     this._initMediaQuerySubscription();
-  }
-
-  ngOnDestroy(): void {
-    this._mediaQueryDestroy$.complete();
   }
 }
