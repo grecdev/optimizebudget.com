8976ba39b3b02425eef47e347c9bfe13191889e5
Grecu Alexandru <grecualexandru001@gmail.com>
2026-07-24 14:26:34 +0300

    feat(OBT-34): sign out button inside user-avatar (mockup)

--- src/app/core/layout/header/user-avatar/sign-out-button/sign-out-button.module.ts
+++ src/app/core/layout/header/user-avatar/sign-out-button/sign-out-button.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AppButtonModule } from '@shared/components/button/button.module';
+
 import { AppSignOutButtonComponent } from './sign-out-button.component';
 
 @NgModule({
   declarations: [AppSignOutButtonComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppButtonModule],
   exports: [AppSignOutButtonComponent],
 })
 export class SignOutButtonModule {}
