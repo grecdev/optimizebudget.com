3b6c1a82659f62d85984d9e077fa26f4c407e1ad
Grecu Alexandru <grecualexandru001@gmail.com>
2026-07-30 19:26:49 +0300

    fix(OBT-34): show only two letters

--- src/app/core/layout/header/user-avatar/pipes/format-name.pipe.ts
+++ src/app/core/layout/header/user-avatar/pipes/format-name.pipe.ts
@@ -7,6 +7,7 @@ export class FormatNamePipe implements PipeTransform {
   transform(value: string): string {
     const VALUE_FORMATTED = value
       .split(' ')
+      .slice(0, 2)
       .map(item => item[0])
       .join('');
 
