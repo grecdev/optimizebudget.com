187e35e6ef093e793d4247bbe3085a8289f7db8e
Grecu Alexandru <grecualexandru001@gmail.com>
2026-08-05 14:53:40 +0300

    fix(branding): logo added in multiple places

--- src/app/core/layout/sidebar/logo/logo.component.ts
+++ src/app/core/layout/sidebar/logo/logo.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LogoComponent {}
--- src/app/features/authentication/components/authentication-logo-container/authentication-logo-container.component.ts
+++ src/app/features/authentication/components/authentication-logo-container/authentication-logo-container.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-authentication-logo-container',
   templateUrl: './authentication-logo-container.component.html',
   styleUrls: ['./authentication-logo-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthenticationLogoContainerComponent {}
