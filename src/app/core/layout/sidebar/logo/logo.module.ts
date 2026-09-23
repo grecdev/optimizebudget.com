109b2383200b6072590e9f4571c83433f6f19be0
Grecu Alexandru <grecualexandru001@gmail.com>
2026-09-23 15:26:10 +0300

    fix(logo): layout missplaced on logo render, affecting canvas

--- src/app/core/layout/sidebar/logo/logo.module.ts
+++ src/app/core/layout/sidebar/logo/logo.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 import { LogoComponent } from './logo.component';
 
 @NgModule({
   declarations: [LogoComponent],
+  imports: [NgOptimizedImage],
   exports: [LogoComponent],
 })
 export class AppSidebarLogoModule {}
